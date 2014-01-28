cd26b5c7656668f57a94f98571037c406473e66c
unknown-user <unknown-user@172.16.139.1>
2014-01-28 14:24:08 +0100

    Remove Amazon EC2

--- AWSClientFactory.cs
+++ AWSClientFactory.cs
@@ -30,7 +30,7 @@ using Amazon.Runtime;
 //using Amazon.DataPipeline;
 //using Amazon.DirectConnect;
 //using Amazon.DynamoDBv2;
-using Amazon.EC2;
+//using Amazon.EC2;
 //using Amazon.ElasticBeanstalk;
 //using Amazon.ElastiCache;
 //using Amazon.ElasticLoadBalancing;
@@ -70,164 +70,164 @@ namespace Amazon
     {
         
 
-        #region Amazon EC2
-
-        /// <summary>
-        /// Create a client for the Amazon EC2 Service with the credentials loaded from the application's
-        /// default configuration, and if unsuccessful from the Instance Profile service on an EC2 instance.
-        /// 
-        /// Example App.config with credentials set. 
-        /// <code>
-        /// &lt;?xml version="1.0" encoding="utf-8" ?&gt;
-        /// &lt;configuration&gt;
-        ///     &lt;appSettings&gt;
-        ///         &lt;add key="AWSAccessKey" value="********************"/&gt;
-        ///         &lt;add key="AWSSecretKey" value="****************************************"/&gt;
-        ///     &lt;/appSettings&gt;
-        /// &lt;/configuration&gt;
-        /// </code>
-        /// </summary>
-        /// <returns>An Amazon EC2 client</returns>
-        /// <remarks>
-        /// </remarks>
-        public static IAmazonEC2 CreateAmazonEC2Client()
-        {
-            return new AmazonEC2Client();
-        }
-
-        /// <summary>
-        /// Create a client for the Amazon EC2 Service with the credentials loaded from the application's
-        /// default configuration, and if unsuccessful from the Instance Profile service on an EC2 instance.
-        /// 
-        /// Example App.config with credentials set. 
-        /// <code>
-        /// &lt;?xml version="1.0" encoding="utf-8" ?&gt;
-        /// &lt;configuration&gt;
-        ///     &lt;appSettings&gt;
-        ///         &lt;add key="AWSAccessKey" value="********************"/&gt;
-        ///         &lt;add key="AWSSecretKey" value="****************************************"/&gt;
-        ///     &lt;/appSettings&gt;
-        /// &lt;/configuration&gt;
-        /// </code>
-        /// </summary>
-        /// <param name="config">Configuration options for the service like HTTP Proxy, # of connections, etc</param>
-        /// <returns>An Amazon EC2 client</returns>
-        public static IAmazonEC2 CreateAmazonEC2Client(AmazonEC2Config config)
-        {
-            return new AmazonEC2Client(config);
-        }
-
-        /// <summary>
-        /// Create a client for the Amazon EC2 Service with the default configuration
-        /// </summary>
-        /// <param name="awsAccessKey">The AWS Access Key associated with the account</param>
-        /// <param name="awsSecretAccessKey">The AWS Secret Access Key associated with the account</param>
-        /// <returns>An Amazon EC2 client</returns>
-        /// <remarks>
-        /// </remarks>
-        public static IAmazonEC2 CreateAmazonEC2Client(
-            string awsAccessKey,
-            string awsSecretAccessKey
-            )
-        {
-            return new AmazonEC2Client(awsAccessKey, awsSecretAccessKey);
-        }
-
-        /// <summary>
-        /// Create a client for the Amazon EC2 Service with the specified configuration
-        /// </summary>
-        /// <param name="awsAccessKey">The AWS Access Key associated with the account</param>
-        /// <param name="awsSecretAccessKey">The AWS Secret Access Key associated with the account</param>
-        /// <param name="config">Configuration options for the service like HTTP Proxy, # of connections, etc
-        /// </param>
-        /// <returns>An Amazon EC2 client</returns>
-        /// <remarks>
-        /// </remarks>
-        public static IAmazonEC2 CreateAmazonEC2Client(
-            string awsAccessKey,
-            string awsSecretAccessKey, AmazonEC2Config config
-            )
-        {
-            return new AmazonEC2Client(awsAccessKey, awsSecretAccessKey, config);
-        }
-
-        /// <summary>
-        /// Create a client for the Amazon EC2 Service with the credentials loaded from the application's
-        /// default configuration, and if unsuccessful from the Instance Profile service on an EC2 instance.
-        /// 
-        /// Example App.config with credentials set. 
-        /// <code>
-        /// &lt;?xml version="1.0" encoding="utf-8" ?&gt;
-        /// &lt;configuration&gt;
-        ///     &lt;appSettings&gt;
-        ///         &lt;add key="AWSAccessKey" value="********************"/&gt;
-        ///         &lt;add key="AWSSecretKey" value="****************************************"/&gt;
-        ///     &lt;/appSettings&gt;
-        /// &lt;/configuration&gt;
-        /// </code>
-        /// </summary>
-        /// <param name="region">The region to connect to.</param>
-        /// <returns>An Amazon EC2 client</returns>
-        public static IAmazonEC2 CreateAmazonEC2Client(RegionEndpoint region)
-        {
-            return new AmazonEC2Client(region);
-        }
-
-        /// <summary>
-        /// Create a client for the Amazon EC2 Service with the specified region
-        /// </summary>
-        /// <param name="awsAccessKey">The AWS Access Key associated with the account</param>
-        /// <param name="awsSecretAccessKey">The AWS Secret Access Key associated with the account</param>
-        /// <param name="region">The region to connect to.</param>
-        /// <returns>An Amazon EC2 client</returns>
-        /// <remarks>
-        /// </remarks>
-        public static IAmazonEC2 CreateAmazonEC2Client(
-            string awsAccessKey,
-            string awsSecretAccessKey, RegionEndpoint region
-            )
-        {
-            return new AmazonEC2Client(awsAccessKey, awsSecretAccessKey, region);
-        }
-
-        /// <summary>
-        /// Create a client for the Amazon EC2 Service with AWSCredentials.
-        /// </summary>
-        /// <param name="credentials">AWS Credentials</param>
-        /// <returns>An Amazon EC2 client</returns>
-        /// <remarks>
-        /// </remarks>
-        public static IAmazonEC2 CreateAmazonEC2Client(AWSCredentials credentials)
-        {
-            return new AmazonEC2Client(credentials);
-        }
-
-        /// <summary>
-        /// Create a client for the Amazon EC2 Service with AWSCredentials and region.
-        /// </summary>
-        /// <param name="credentials">AWS Credentials</param>
-        /// <param name="region">The region to connect to.</param>
-        /// <returns>An Amazon EC2 client</returns>
-        /// <remarks>
-        /// </remarks>
-        public static IAmazonEC2 CreateAmazonEC2Client(AWSCredentials credentials, RegionEndpoint region)
-        {
-            return new AmazonEC2Client(credentials, region);
-        }
-
-        /// <summary>
-        /// Create a client for the Amazon EC2 Service with AWSCredentials and an AmazonEC2 Configuration object.
-        /// </summary>
-        /// <param name="credentials">AWS Credentials</param>
-        /// <param name="config">Configuration options for the service like HTTP Proxy, # of connections, etc</param>
-        /// <returns>An Amazon EC2 client</returns>
-        /// <remarks>
-        /// </remarks>
-        public static IAmazonEC2 CreateAmazonEC2Client(AWSCredentials credentials, AmazonEC2Config config)
-        {
-            return new AmazonEC2Client(credentials, config);
-        }
-        #endregion
+//        #region Amazon EC2
+//
+//        /// <summary>
+//        /// Create a client for the Amazon EC2 Service with the credentials loaded from the application's
+//        /// default configuration, and if unsuccessful from the Instance Profile service on an EC2 instance.
+//        /// 
+//        /// Example App.config with credentials set. 
+//        /// <code>
+//        /// &lt;?xml version="1.0" encoding="utf-8" ?&gt;
+//        /// &lt;configuration&gt;
+//        ///     &lt;appSettings&gt;
+//        ///         &lt;add key="AWSAccessKey" value="********************"/&gt;
+//        ///         &lt;add key="AWSSecretKey" value="****************************************"/&gt;
+//        ///     &lt;/appSettings&gt;
+//        /// &lt;/configuration&gt;
+//        /// </code>
+//        /// </summary>
+//        /// <returns>An Amazon EC2 client</returns>
+//        /// <remarks>
+//        /// </remarks>
+//        public static IAmazonEC2 CreateAmazonEC2Client()
+//        {
+//            return new AmazonEC2Client();
+//        }
+//
+//        /// <summary>
+//        /// Create a client for the Amazon EC2 Service with the credentials loaded from the application's
+//        /// default configuration, and if unsuccessful from the Instance Profile service on an EC2 instance.
+//        /// 
+//        /// Example App.config with credentials set. 
+//        /// <code>
+//        /// &lt;?xml version="1.0" encoding="utf-8" ?&gt;
+//        /// &lt;configuration&gt;
+//        ///     &lt;appSettings&gt;
+//        ///         &lt;add key="AWSAccessKey" value="********************"/&gt;
+//        ///         &lt;add key="AWSSecretKey" value="****************************************"/&gt;
+//        ///     &lt;/appSettings&gt;
+//        /// &lt;/configuration&gt;
+//        /// </code>
+//        /// </summary>
+//        /// <param name="config">Configuration options for the service like HTTP Proxy, # of connections, etc</param>
+//        /// <returns>An Amazon EC2 client</returns>
+//        public static IAmazonEC2 CreateAmazonEC2Client(AmazonEC2Config config)
+//        {
+//            return new AmazonEC2Client(config);
+//        }
+//
+//        /// <summary>
+//        /// Create a client for the Amazon EC2 Service with the default configuration
+//        /// </summary>
+//        /// <param name="awsAccessKey">The AWS Access Key associated with the account</param>
+//        /// <param name="awsSecretAccessKey">The AWS Secret Access Key associated with the account</param>
+//        /// <returns>An Amazon EC2 client</returns>
+//        /// <remarks>
+//        /// </remarks>
+//        public static IAmazonEC2 CreateAmazonEC2Client(
+//            string awsAccessKey,
+//            string awsSecretAccessKey
+//            )
+//        {
+//            return new AmazonEC2Client(awsAccessKey, awsSecretAccessKey);
+//        }
+//
+//        /// <summary>
+//        /// Create a client for the Amazon EC2 Service with the specified configuration
+//        /// </summary>
+//        /// <param name="awsAccessKey">The AWS Access Key associated with the account</param>
+//        /// <param name="awsSecretAccessKey">The AWS Secret Access Key associated with the account</param>
+//        /// <param name="config">Configuration options for the service like HTTP Proxy, # of connections, etc
+//        /// </param>
+//        /// <returns>An Amazon EC2 client</returns>
+//        /// <remarks>
+//        /// </remarks>
+//        public static IAmazonEC2 CreateAmazonEC2Client(
+//            string awsAccessKey,
+//            string awsSecretAccessKey, AmazonEC2Config config
+//            )
+//        {
+//            return new AmazonEC2Client(awsAccessKey, awsSecretAccessKey, config);
+//        }
+//
+//        /// <summary>
+//        /// Create a client for the Amazon EC2 Service with the credentials loaded from the application's
+//        /// default configuration, and if unsuccessful from the Instance Profile service on an EC2 instance.
+//        /// 
+//        /// Example App.config with credentials set. 
+//        /// <code>
+//        /// &lt;?xml version="1.0" encoding="utf-8" ?&gt;
+//        /// &lt;configuration&gt;
+//        ///     &lt;appSettings&gt;
+//        ///         &lt;add key="AWSAccessKey" value="********************"/&gt;
+//        ///         &lt;add key="AWSSecretKey" value="****************************************"/&gt;
+//        ///     &lt;/appSettings&gt;
+//        /// &lt;/configuration&gt;
+//        /// </code>
+//        /// </summary>
+//        /// <param name="region">The region to connect to.</param>
+//        /// <returns>An Amazon EC2 client</returns>
+//        public static IAmazonEC2 CreateAmazonEC2Client(RegionEndpoint region)
+//        {
+//            return new AmazonEC2Client(region);
+//        }
+//
+//        /// <summary>
+//        /// Create a client for the Amazon EC2 Service with the specified region
+//        /// </summary>
+//        /// <param name="awsAccessKey">The AWS Access Key associated with the account</param>
+//        /// <param name="awsSecretAccessKey">The AWS Secret Access Key associated with the account</param>
+//        /// <param name="region">The region to connect to.</param>
+//        /// <returns>An Amazon EC2 client</returns>
+//        /// <remarks>
+//        /// </remarks>
+//        public static IAmazonEC2 CreateAmazonEC2Client(
+//            string awsAccessKey,
+//            string awsSecretAccessKey, RegionEndpoint region
+//            )
+//        {
+//            return new AmazonEC2Client(awsAccessKey, awsSecretAccessKey, region);
+//        }
+//
+//        /// <summary>
+//        /// Create a client for the Amazon EC2 Service with AWSCredentials.
+//        /// </summary>
+//        /// <param name="credentials">AWS Credentials</param>
+//        /// <returns>An Amazon EC2 client</returns>
+//        /// <remarks>
+//        /// </remarks>
+//        public static IAmazonEC2 CreateAmazonEC2Client(AWSCredentials credentials)
+//        {
+//            return new AmazonEC2Client(credentials);
+//        }
+//
+//        /// <summary>
+//        /// Create a client for the Amazon EC2 Service with AWSCredentials and region.
+//        /// </summary>
+//        /// <param name="credentials">AWS Credentials</param>
+//        /// <param name="region">The region to connect to.</param>
+//        /// <returns>An Amazon EC2 client</returns>
+//        /// <remarks>
+//        /// </remarks>
+//        public static IAmazonEC2 CreateAmazonEC2Client(AWSCredentials credentials, RegionEndpoint region)
+//        {
+//            return new AmazonEC2Client(credentials, region);
+//        }
+//
+//        /// <summary>
+//        /// Create a client for the Amazon EC2 Service with AWSCredentials and an AmazonEC2 Configuration object.
+//        /// </summary>
+//        /// <param name="credentials">AWS Credentials</param>
+//        /// <param name="config">Configuration options for the service like HTTP Proxy, # of connections, etc</param>
+//        /// <returns>An Amazon EC2 client</returns>
+//        /// <remarks>
+//        /// </remarks>
+//        public static IAmazonEC2 CreateAmazonEC2Client(AWSCredentials credentials, AmazonEC2Config config)
+//        {
+//            return new AmazonEC2Client(credentials, config);
+//        }
+//        #endregion
 
         
 
